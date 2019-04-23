4296a9e590a5f94c015bf23e42ec5c131d71a555
Vladimir Rodkin <mail@vovanr.com>
2019-04-23 13:41:12 +0300

    AT new task: Parse task data and add automatically

--- src/at-new-task.js
+++ src/at-new-task.js
@@ -1,9 +1,43 @@
 (function() {
-  document.getElementById("addTasksLink").dispatchEvent(new Event("click"));
+  let title = "";
+  let url = "";
 
-  setTimeout(function() {
-    document
-      .getElementById("descriptionElement")
-      .dispatchEvent(new Event("click"));
+  const getAddNewTaskButtonElement = () =>
+    document.querySelector("#addTasksLink");
+
+  const getAddTaskDescriptionButtonElement = () =>
+    document.querySelector("#descriptionElement");
+
+  const getDescriptionElement = () =>
+    document.querySelector("#editDescriptionPopupText");
+
+  const getNameElement = () =>
+    document.querySelector("#createTasksPopup .nameCell.first input");
+
+  const getCreateTaskButtonElement = () =>
+    document.querySelector("#createTasksPopup .components_button");
+
+  getAddNewTaskButtonElement().click();
+
+  function listener({ target: { value } }) {
+    [title, url] = value.split("\n");
+
+    getDescriptionElement().removeEventListener("input", listener);
+    getDescriptionElement().value = url;
+
+    getNameElement().value = title;
+
+    document.querySelector("#scbutton").click();
+
+    getNameElement().click();
+    getNameElement().dispatchEvent(new Event("blur"));
+
+    getCreateTaskButtonElement().click();
+  }
+
+  setTimeout(() => {
+    getAddTaskDescriptionButtonElement().click();
+
+    getDescriptionElement().addEventListener("input", listener);
   }, 1000);
 })();
